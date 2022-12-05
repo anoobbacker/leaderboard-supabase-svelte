365a039b0a4c4987d20c7d9c680f61828f014b73
nine-long <ninetydays.work@outlook.com>
2022-12-05 14:58:25 +0000

    Main adding tile format

---
+++ src/lib/ErrorMessage.svelte
@@ -0,0 +1,12 @@
+<div class="cover-container d-flex text-center p-3 flex-column">
+  <div class="row min-vh-100" >
+    <div class="d-flex align-items-center">
+      <div class="mx-auto">
+        <h1 class="display-5 fw-bold">That's an error</h1>
+        <p class="lead mb-4">The requested operation could not processed! That's all we know.</p>
+        <img class="rounded w-25 p-3" src="/assets/img/404.png" alt="404" />
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</div>
